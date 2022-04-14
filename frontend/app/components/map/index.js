3b1e829fc381a5f584aed09c63846e1f1f08f7aa
user <info@d-element.ru>
2022-04-14 20:06:06 +0500

    объект карты

--- frontend/app/components/map/index.js
+++ frontend/app/components/map/index.js
@@ -1,10 +1,30 @@
 import "./style.pcss";
 import Collection from "../../js/collection";
 import {getJS} from "../../js/utils/getJS";
+import {getConfig} from "../../js/utils/getConfig";
 
 export class Map {
-  constructor() {
+  constructor(instance) {
+    this.instance = instance
+    this.map = null
+    this.clusterer = null
+    this.cfg = getConfig(this.instance, MapsCollection.instance)
+    this.bindEvents()
+  }
 
+  init() {
+    this.map = new ymaps.Map(this.instance, this.cfg)
+    this.clusterer = new ymaps.Clusterer()
+    this.clusterer.add(this.getPoints())
+    this.map.geoObjects.add(this.clusterer)
+  }
+
+  getPoints() {
+    return this.cfg.points.map(({coords, icon}) =>  new ymaps.Placemark(coords), {} )
+  }
+
+  bindEvents() {
+    window.ymaps.ready(() => this.init())
   }
 }
 
@@ -19,7 +39,9 @@ export default class MapsCollection extends Collection {
   static urlAPI = "https://api-maps.yandex.ru/2.1/?apikey=&lang=ru_RU";
 
   init() {
-
+    document.querySelectorAll(MapsCollection.instance).forEach(el => {
+      this.collection = new Map(el)
+    })
   }
 
   load() {
